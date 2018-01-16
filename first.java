8a9f38cf6beb8e98e77d98a1efd1450274f9d07a
harishbalasubramanian <harishbala153@gmail.com>
2018-01-16 15:04:03 -0800

    Update first.java

--- first.java
+++ first.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class first {
 	public static void main(String[] args){
 		second sec = new second();
-		sec.setSize(500,900);
+		sec.setSize(550,800);
 		sec.setVisible(true);
 		sec.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
